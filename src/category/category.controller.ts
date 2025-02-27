9db73313253771b55319178912b487ac6afc8496
NamDuong1212 <nguyennamduong1212@gmail.com>
2025-02-27 11:16:28 +0700

    update category fix

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -15,31 +15,10 @@ import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { ApiKeyGuard } from 'src/auth/guard/api-key.guard';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
-
-  @ApiTags('Category')
-  @ApiBearerAuth('api-key')
-  @ApiOperation({
-    summary: 'Create a new category',
-    description: 'Allows a creator to create a new category.',
-  })
-  @ApiResponse({
-    status: 201,
-    description: 'Category created successfully.',
-    type: CreateCategoryDto,
-  })
-  @Post('/create')
-  @UseGuards(ApiKeyGuard)
-  async createCategory(
-    @Request() req,
-    @Body() createCategoryDto: CreateCategoryDto,
-  ) {
-    const category =
-      await this.categoryService.createCategory(createCategoryDto);
-    return { message: 'Category created successfully', data: category };
-  }
 
   @ApiTags('Category')
   @ApiBearerAuth('')
@@ -67,47 +46,5 @@ export class CategoryController {
   async getCategoryById(@Param('id') id: string) {
     const category = await this.categoryService.getCategoryById(id);
     return { data: category };
-  }
-
-  @ApiTags('Category')
-  @ApiBearerAuth('api-key')
-  @ApiOperation({ summary: 'Update a category' })
-  @ApiResponse({
-    status: 200,
-    description: 'Category updated successfully.',
-    type: UpdateCategoryDto,
-  })
-  @Patch('/:id')
-  @UseGuards(ApiKeyGuard)
-  async updateCategory(
-    @Request() req,
-    @Param('id') id: string,
-    @Body() updateCategoryDto: UpdateCategoryDto,
-  ) {
-    const user = req.user;
-
-    if (!user || !user.isCreator) {
-      throw new UnauthorizedException('Access denied. Creator only.');
-    }
-    const updateCategory = await this.categoryService.updateCategory(
-      id,
-      updateCategoryDto,
-    );
-    return { data: updateCategory };
-  }
-
-  @ApiTags('Category')
-  @ApiBearerAuth('')
-  @ApiOperation({ summary: 'Delete a category' })
-  @ApiResponse({
-    status: 200,
-    description: 'Category deleted successfully.'
-  })
-  @Delete('/:id')
-  @UseGuards(ApiKeyGuard)
-  async deleteCategory(@Param('id') id: string, @Request() req) {
-    const isCreator = req.user?.isCreator;
-    const category = await this.categoryService.deleteCategory(id, isCreator);
-    return category;
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -6,8 +6,6 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './category.entity';
 import { Repository } from 'typeorm';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -15,14 +13,6 @@ export class CategoryService {
     @InjectRepository(Category)
     private readonly categoryRepository: Repository<Category>,
   ) {}
-
-  async createCategory(
-    createCategoryDto: CreateCategoryDto,
-  ): Promise<Category> {
-    const category = this.categoryRepository.create(createCategoryDto);
-    await this.categoryRepository.save(category);
-    return category;
-  }
 
   async getAllCategory(): Promise<Category[]> {
     return await this.categoryRepository.find();
@@ -38,36 +28,4 @@ export class CategoryService {
     return category;
   }
 
-  async updateCategory(
-    id: string,
-    updateCategoryDto: UpdateCategoryDto,
-  ): Promise<Category> {
-    const category = await this.categoryRepository.findOne({ where: { id } });
-
-    if (!category) {
-      throw new NotFoundException('Category not found');
-    }
-
-    const updateCategory = Object.assign(category, updateCategoryDto);
-    return this.categoryRepository.save(updateCategory);
-  }
-
-  async deleteCategory(
-    id: string,
-    isCreator: boolean,
-  ): Promise<{ message: string }> {
-    if (!isCreator) {
-      throw new UnauthorizedException('Access denied. Creator only.');
-    }
-
-    const category = await this.categoryRepository.findOne({ where: { id } });
-
-    if (!category) {
-      throw new NotFoundException('Category not found');
-    }
-
-    await this.categoryRepository.remove(category);
-
-    return { message: 'Category deleted successfully' };
-  }
 }
